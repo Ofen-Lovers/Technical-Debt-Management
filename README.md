6c951cf63f203a883d85ccacda5f06e9bb81cab9
Shai290 <dadios.shaira@gmail.com>
2025-03-06 17:27:39 +0800

    Added unit testing and Error Handling, Tester & Documenter

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Technical-Debt-Management
 A repository made for the Software Implementations and Management Activity
+
+
+Test Cases:
+    1. Testing String Input
+    2.
--- salaryDeduction.py
+++ salaryDeduction.py
@@ -7,8 +7,9 @@ def compute_deductions(salary):
     # SSS Exemption for minimum wage earners (salary below 8250)
     if salary < 8250:
         sss = 0
-
-    #Deductions and net salary computations
+    if salary < 25000:
+        tax = 0
+    # Deductions and net salary computations
     deductions = sss + philhealth + pagibig + tax
     net_salary = salary - deductions
 
@@ -16,13 +17,16 @@ def compute_deductions(salary):
 
 def main():
     try:
-        salary = float(input("Enter your monthly salary: ")) #User input for salary
+        salary = float(input("Enter your monthly salary: ")) # User input for salary
+        if salary < 0:
+            print("Salary cannot be negative. Please enter a valid salary.")
+            return
         print("Gross Salary:", salary)
     except ValueError:
         print("Please enter a valid number.")
         return
-
-    #Function call to compute deductions and net salary
+    
+    # Function call to compute deductions and net salary
     sss, philhealth, pagibig, tax, deductions, net_salary = compute_deductions(salary)
 
     print("\n-----Deductables-----")
@@ -32,7 +36,3 @@ def main():
     print("Tax Deduction:", tax)
     print("Total Deductions:", deductions)
     print("\nNet Salary:", net_salary)
-
-if __name__ == "__main__":
-    main()
-
